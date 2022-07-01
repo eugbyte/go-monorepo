e2dca8734ee636201be3452933e8f9db49cca9c8
eugbyte <eugenetham1994@gmail.com>
2022-07-01 18:12:38 +0800

    chore: update

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	hello "github.com/web-notify-lib/notify-api-azure/hello_handler"
 	"github.com/web-notify-lib/notify-api-azure/lib/config"
@@ -12,14 +11,11 @@ import (
 
 func main() {
 	var address string = config.Config.LOCAL_PORT
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		address = val
-	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/hello", hello.Handler)
 
-	log.Printf("About to listen on %s. Go to https://localhost:%s/", address, address)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1:%s/", address, address)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("127.0.0.1:%s", address), mux))
 
 }
