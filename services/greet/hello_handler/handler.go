e08408adc058effaa4fe5f8e9c0446cde3490d51
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-07-24 20:51:56 +0800

    chore: revert greet handler

--- services/greet/hello_handler/handler.go
+++ services/greet/hello_handler/handler.go
@@ -3,9 +3,6 @@ package hello_handler
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/web-notify/api/monorepo/libs/store/vault"
-	"github.com/web-notify/api/monorepo/libs/utils/formats"
 )
 
 type RequestBody struct {
@@ -18,21 +15,10 @@ func Handler(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	secretName := "db_Name"
-	secretValue := "123"
-
-	vs := vault.NewVaultService("https://localhost:8443")
-	err := vs.SetSecret(secretName, secretValue)
-	if err != nil {
-		http.Error(response, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	formats.Trace(secretName, secretValue, vs)
-
 	responseBody := map[string]interface{}{"message": "Hello World"}
 
 	response.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	err = json.NewEncoder(response).Encode(responseBody)
+	err := json.NewEncoder(response).Encode(responseBody)
 	if err != nil {
 		http.Error(response, err.Error(), http.StatusInternalServerError)
 		return
