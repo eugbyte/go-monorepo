c30d29b59fb6037a78ed3677854e3a9161152fd6
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-07-25 14:38:17 +0800

    chore: update

--- services/greet/main.go
+++ services/greet/main.go
@@ -16,10 +16,8 @@ func main() {
 	var stage config.STAGE = config.Stage()
 	var address string = config.ENV_VARS[stage].LOCAL_PORT
 	mux := http.NewServeMux()
-	mux.Handle("/api/hello", middlewares.Middy(http.HandlerFunc(hello.Handler), loggerMW.LoggerMiddleware))
-
-	wrappedMux := cors.Default().Handler(mux)
+	mux.Handle("/api/hello", middlewares.Middy(http.HandlerFunc(hello.Handler), loggerMW.LoggerMiddleware, cors.Default().Handler))
 
 	log.Printf("About to listen on %s. Go to https://127.0.0.1:%s/", address, address)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("127.0.0.1:%s", address), wrappedMux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("127.0.0.1:%s", address), mux))
 }
