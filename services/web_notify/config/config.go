0288401c3f33cb88d8c022bef749a767208569f4
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-22 11:04:42 +0800

    chore: update

--- services/web_notify/config/config.go
+++ services/web_notify/config/config.go
@@ -9,11 +9,9 @@ type vars struct {
 	QUEUE_ACCOUNT_NAME         string
 	QUEUE_ACCOUNT_KEY          string
 	MONGO_DB_CONNECTION_STRING string
-	VAPID_PUBLIC_KEY           string
-	VAPID_SENDER_EMAIL         string
 	VAULT_URI                  string
 	VAULT_URI_COMPANY          string
-	NOTIFY_PRODUCER_URL        string
+	NOTIFY_BASE_URL            string
 }
 
 // redeclare variables to avoid confusion between current config pkg and configlib pkg
@@ -36,22 +34,18 @@ func New() vars {
 			QUEUE_ACCOUNT_NAME:         "devstoreaccount1",
 			QUEUE_ACCOUNT_KEY:          "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
 			MONGO_DB_CONNECTION_STRING: `mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true`,
-			VAPID_PUBLIC_KEY:           "BPlL5OTZwtW-0-4pQXmobTgX6URszc9-UKoTTvpvInhUlPHorlDM8y04J-rrErlQXMVH7_Us983mNmmwsb-z53U",
-			VAPID_SENDER_EMAIL:         "eugenetham1994@gmail.com",
 			VAULT_URI:                  "https://kv-notify-secrets-stg-ea.vault.azure.net",
 			VAULT_URI_COMPANY:          "https://kv-notify-company-stg-ea.vault.azure.net",
-			NOTIFY_PRODUCER_URL:        "http://localhost:7071/api/notifications",
+			NOTIFY_BASE_URL:            "http://localhost:7071/api",
 		},
 		STAGING: {
 			LOCAL_PORT:                 EnvOrDefault("FUNCTIONS_CUSTOMHANDLER_PORT", ""),
 			QUEUE_ACCOUNT_NAME:         EnvOrDefault("QUEUE_ACCOUNT_NAME", ""),
 			QUEUE_ACCOUNT_KEY:          EnvOrDefault("QUEUE_ACCOUNT_KEY", ""),
 			MONGO_DB_CONNECTION_STRING: EnvOrDefault("MONGO_DB_CONNECTION_STRING", ""),
-			VAPID_PUBLIC_KEY:           "BPlL5OTZwtW-0-4pQXmobTgX6URszc9-UKoTTvpvInhUlPHorlDM8y04J-rrErlQXMVH7_Us983mNmmwsb-z53U",
-			VAPID_SENDER_EMAIL:         "eugenetham1994@gmail.com",
 			VAULT_URI:                  EnvOrDefault("VAULT_URI", ""),
 			VAULT_URI_COMPANY:          EnvOrDefault("VAULT_URI_COMPANY", ""),
-			NOTIFY_PRODUCER_URL:        EnvOrDefault("NOTIFY_PRODUCER_URL", "http://localhost:7071/api/notifications"),
+			NOTIFY_BASE_URL:            EnvOrDefault("NOTIFY_BASE_URL", ""),
 		},
 	}
 	env_vars[PROD] = env_vars[STAGING]
